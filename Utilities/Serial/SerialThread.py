2d78006987d3c347100a960d80fb638866ca16e7
LeeGingko <1596510773@qq.com>
2021-04-01 22:47:31 +0800

    发现下发参数总是不成功 1 单独调试两边发现发送都没问题，于是发现工作模式处理线程会使得参数接收紊乱，R出现在帧头更改下位机代码解决 2 串口接收线程莫名其妙工作起来了。。。 3 模式检查 编码 检测暂无问题 4 开启串口后进行控制仪检测应答、自检

--- Utilities/Serial/SerialThread.py
+++ Utilities/Serial/SerialThread.py
@@ -16,8 +16,13 @@ class PersonalSerial(QThread):
 
     def __init__(self):
         super(PersonalSerial, self).__init__()
-        global userSerial
         self.userSerial = serial.Serial()
+        
+    def  __del__(self):
+        self.wait()
+
+    def initPort(self, port):
+        self.userSerial.port = port
         self.userSerial.baudrate = 115200
         self.userSerial.bytesize = serial.EIGHTBITS
         self.userSerial.stopbits = serial.STOPBITS_ONE
@@ -27,12 +32,6 @@ class PersonalSerial(QThread):
         self.userSerial.rtscts = False
         self.userSerial.dsrdtr = False
 
-    def  __del__(self):
-        self.wait()
-
-    def initPort(self, port):
-        self.userSerial.port = port
-
     def run(self):
         while True:
             QApplication.processEvents()
@@ -40,16 +39,20 @@ class PersonalSerial(QThread):
             self.data = b""
             tmp = ""
             if self.userSerial.isOpen():
-                self.num = self.userSerial.inWaiting()
-                # print(self.num)
-                if self.num > 0 and self.num <= 4:
-                    self.userSerial.flushInput()
-                elif self.num >= 9:
-                    time.sleep(0.01)
+                try:
                     self.num = self.userSerial.inWaiting()
-                    # print("PersonalSerial->run->" + str(self.num)) # 输出收到的字节数
-                    self.data = self.userSerial.read(self.num)
-                    tmp = self.data.decode("utf-8")
-                    # print(self.data.decode("utf-8"))
-                    if tmp[0] == "R" and tmp[7] == "\r" and tmp[8] == "\n":
-                        self.recvSignal.emit(tmp[2:5])
+                    # print(self.num)
+                    if self.num > 0 and self.num <= 4:
+                        self.userSerial.flushInput()
+                    elif self.num == 6:
+                        time.sleep(0.01)
+                        self.num = self.userSerial.inWaiting()
+                        # print("PersonalSerial->run->" + str(self.num)) # 输出收到的字节数
+                        if self.num == 6:
+                            self.data = self.userSerial.read(self.num)
+                            tmp = self.data.decode("utf-8")
+                            print(self.data.decode("utf-8"))
+                            if tmp[0] == "R" and tmp[4] == "\r" and tmp[5] == "\n":
+                                self.recvSignal.emit(tmp[1:4])
+                except:
+                    pass
